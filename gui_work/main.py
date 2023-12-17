0cad46fc9b91d09439fd6f5d5dcd576d68c5780a
Adam <adam15free@hotmail.com>
2023-12-17 09:15:05 -0500

    Update IP address and copy directories for Splunk forwarder

---
+++ gui_work/main.py
@@ -0,0 +1,24 @@
+import pyautogui
+import random
+
+type_interval = random.uniform(0.1, 0.5)
+# Get the size of the primary monitor.
+screenWidth, screenHeight = pyautogui.size()
+
+# Move the mouse to XY coordinates.
+pyautogui.moveTo(100, 150, duration=0.25)
+
+# Click the mouse.
+pyautogui.click(100, 150, duration=0.25, button='right')
+
+# Type a string using the keyboard.
+pyautogui.write('Hello world!', interval=type_interval)  # type with quarter-second pause in between each key
+
+# Press the 'esc' key.
+pyautogui.press('esc')
+
+# Press the 'ctrl-c' hotkey combination.
+pyautogui.hotkey('ctrl', 'c')
+
+# Press the 'enter' key.
+pyautogui.press('enter')
