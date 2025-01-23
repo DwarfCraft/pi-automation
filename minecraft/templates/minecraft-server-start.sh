1f9a21a36a3c4435c46e0a0579bf0f1afed4bf81
Adam Holmes <Adam15Free@hotmail.com>
2025-01-23 09:30:11 -0500

    Add mod installation and improve server start/stop scripts

--- minecraft/templates/minecraft-server-start.sh
+++ minecraft/templates/minecraft-server-start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-tmux new-session -d -s ${TMUX_SESSION} -n ${TMUX_WINDOW} "/usr/bin/java ${JVM_OPTS} -jar {{ minecraft_home }}/server.jar nogui"
+# Set default values if environment variables do not exist
+TMUX_SESSION="${TMUX_SESSION:-minecraft}"
+TMUX_WINDOW="${TMUX_WINDOW:-fabric}"
+JVM_OPTS="${JVM_OPTS:--server -XX:+UseConcMarkSweepGC -XX:MaxGCPauseMillis=50 -Xmx3G}"
+minecraft_home="${minecraft_home:-/opt/minecraft}"
 
+tmux new-session -d -s "${TMUX_SESSION}" -n "${TMUX_WINDOW}" "/usr/bin/java ${JVM_OPTS} -jar ${minecraft_home}/server.jar nogui"
--- minecraft/templates/minecraft-server-stop.sh
+++ minecraft/templates/minecraft-server-stop.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-tmux send -t minecraft "stop"
-tmux send -t minecraft "Enter"
+# Set default values if environment variables do not exist
+TMUX_SESSION="${TMUX_SESSION:-minecraft}"
+TMUX_WINDOW="${TMUX_WINDOW:-fabric}"
+
+tmux send -t "${TMUX_SESSION}:${TMUX_WINDOW}" "stop" ENTER
+
 loop=60
 while [ $loop -gt 0 ]; do
     # Loop again unless the tmux window is closed
-    tmux list-window -F '#{window_name}' -t ${TMUX_SESSION} 2>&1 | grep -q ^${TMUX_WINDOW}$
+    tmux list-windows -F '#{window_name}' -t "${TMUX_SESSION}" 2>&1 | grep -q "^${TMUX_WINDOW}$"
     if [ $? = 0 ] ; then
         loop=$((loop - 1))
         sleep 1
