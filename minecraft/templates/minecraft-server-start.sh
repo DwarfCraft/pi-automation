33ef203ac1fffbd18c11e71efb291f72eeaf1362
Adam <adam15free@hotmail.com>
2025-01-27 15:56:02 -0500

    Wait for server to start before setting world spawn in server start script

--- minecraft/templates/minecraft-server-start.sh
+++ minecraft/templates/minecraft-server-start.sh
@@ -6,4 +6,9 @@ JAVA_OPTS="{{ java_opts }}"
 MINECRAFT_HOME="{{ minecraft_home }}"
 
 tmux new-session -d -s "${TMUX_SESSION}" -n "${TMUX_WINDOW}" "/usr/bin/java ${JAVA_OPTS} -jar ${MINECRAFT_HOME}/server.jar nogui"
-tmux send -t "${TMUX_SESSION}:${TMUX_WINDOW}" "setworldspawn 0 0 0" ENTER
+
+# look for this text '[Server thread/INFO]: Done ()! For help, type "help"' in the latest.log to then run the command
+while ! grep -q '[Server thread/INFO]: Done' ${MINECRAFT_HOME}/logs/latest.log; do
+    sleep 1
+done 
+tmux send -t "${TMUX_SESSION}:${TMUX_WINDOW}" "setworldspawn 0 0 0" ENTER   
