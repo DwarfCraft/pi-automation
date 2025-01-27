68fa823c65baa93b6bb1219fc17e24de761f67c6
Adam <adam15free@hotmail.com>
2025-01-27 16:45:56 -0500

    Add comment to clarify server readiness check in setup script

--- minecraft/templates/setup-minecraft-server.sh
+++ minecraft/templates/setup-minecraft-server.sh
@@ -4,7 +4,7 @@ TMUX_SESSION="{{ tmux_session }}"
 TMUX_WINDOW="{{ tmux_window }}"
 MINECRAFT_HOME="{{ minecraft_home }}"
 
-Validate the server has started before running the command.
+# Validate the server has started before running the command.
 # look for this text '[Server thread/INFO]: Done ()! For help, type "help"' in the latest.log to then run the command
 while ! grep -q '\[Server thread\/INFO\]: Done' ${MINECRAFT_HOME}/logs/latest.log; do
     sleep 1
