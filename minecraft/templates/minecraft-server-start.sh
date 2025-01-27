7b9a50340950028cf8010954410ee3958a766af8
Adam <adam15free@hotmail.com>
2025-01-27 16:00:42 -0500

    Improve server start script to include waiting message while checking server readiness

--- minecraft/templates/minecraft-server-start.sh
+++ minecraft/templates/minecraft-server-start.sh
@@ -8,7 +8,8 @@ MINECRAFT_HOME="{{ minecraft_home }}"
 tmux new-session -d -s "${TMUX_SESSION}" -n "${TMUX_WINDOW}" "/usr/bin/java ${JAVA_OPTS} -jar ${MINECRAFT_HOME}/server.jar nogui"
 
 # look for this text '[Server thread/INFO]: Done ()! For help, type "help"' in the latest.log to then run the command
-while ! grep -q '[Server thread/INFO]: Done' ${MINECRAFT_HOME}/logs/latest.log; do
+while ! grep -q '\[Server thread\/INFO\]: Done' ${MINECRAFT_HOME}/logs/latest.log; do
     sleep 1
+    echo "Waiting for server to start..."
 done 
 tmux send -t "${TMUX_SESSION}:${TMUX_WINDOW}" "setworldspawn 0 0 0" ENTER   
