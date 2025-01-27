7e19799c199a5a8b86c72bd364a84a5b34e0f405
Adam <adam15free@hotmail.com>
2025-01-27 15:40:49 -0500

    Add view distance configuration and set world spawn in server start script

--- minecraft/templates/minecraft-server-start.sh
+++ minecraft/templates/minecraft-server-start.sh
@@ -6,3 +6,4 @@ JAVA_OPTS="{{ java_opts }}"
 MINECRAFT_HOME="{{ minecraft_home }}"
 
 tmux new-session -d -s "${TMUX_SESSION}" -n "${TMUX_WINDOW}" "/usr/bin/java ${JAVA_OPTS} -jar ${MINECRAFT_HOME}/server.jar nogui"
+tmux send -t "${TMUX_SESSION}:${TMUX_WINDOW}" "setworldspawn 0 0 0" ENTER
