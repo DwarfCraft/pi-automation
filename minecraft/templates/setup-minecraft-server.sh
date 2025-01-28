62a6bd784d8ddca38bdc9f33560a2b64c28db655
Adam <adam15free@hotmail.com>
2025-01-28 11:59:54 -0500

    Refactor server startup scripts to remove outdated log checks and ensure proper user permissions

--- minecraft/templates/setup-minecraft-server.sh
+++ minecraft/templates/setup-minecraft-server.sh
@@ -5,12 +5,6 @@ TMUX_WINDOW="{{ tmux_window }}"
 MINECRAFT_HOME="{{ minecraft_home }}"
 MINECRAFT_USER="{{ minecraft_user }}"
 
-# Check for '[Server thread/INFO]: Stopping server' in the latest.log to validate we are not looking at an old log file
-while ! grep -q '\[Server thread\/INFO\]: Stopping server' ${MINECRAFT_HOME}/logs/latest.log; do
-    sleep 5
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Waiting for server to reset log file..."
-done
-
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Checking if the server has started..."
 # Validate the server has started before running the command.
 # look for this text '[Server thread/INFO]: Done ()! For help, type "help"' in the latest.log to then run the command
