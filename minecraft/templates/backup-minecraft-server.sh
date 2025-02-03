8b2fdf920b9e78b9d6516ce495d7f1e6e958e8f0
Adam <adam15free@hotmail.com>
2025-02-03 16:02:58 -0500

    Enhance backup script to manage duplicate backups and limit storage usage

--- minecraft/templates/backup-minecraft-server.sh
+++ minecraft/templates/backup-minecraft-server.sh
@@ -15,5 +15,18 @@ tar -czvf $BACKUP_FILE $SERVER_DIR/world $SERVER_DIR/server.properties $SERVER_D
 # Only keep 7 days worth of backups
 find $BACKUP_DIR -type f -name 'minecraft_backup_*' -mtime +7 -exec rm {} \;
 
+# loop through all backups and only keep a single instance if they have the same sha256sum
+for file in $(ls $BACKUP_DIR/minecraft_backup_*); do
+    if [ $(sha256sum $file | cut -d ' ' -f1) == $(sha256sum $BACKUP_FILE | cut -d ' ' -f1) ]; then
+        rm $BACKUP_FILE
+        break
+    fi
+done
+
+# Only keep a max of 20GB of backups
+while [ $(du -s $BACKUP_DIR | cut -f1) -gt 20000 ]; do
+    rm $(ls -t $BACKUP_DIR/minecraft_backup_* | tail -1)
+done
+
 # Print completion message
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Backup completed: $BACKUP_FILE"
