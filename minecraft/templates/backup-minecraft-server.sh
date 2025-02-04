1d2f41176ca3bf80e786d6312da60dbbda2a677d
Adam <adam15free@hotmail.com>
2025-02-04 09:43:19 -0500

    Improve backup script to avoid deleting the current backup when managing duplicates

--- minecraft/templates/backup-minecraft-server.sh
+++ minecraft/templates/backup-minecraft-server.sh
@@ -10,16 +10,17 @@ BACKUP_FILE="$BACKUP_DIR/minecraft_backup_$TIMESTAMP.tar.gz"
 mkdir -p $BACKUP_DIR
 
 # Backup the important parts of the Minecraft server
-tar -czvf $BACKUP_FILE $SERVER_DIR/world $SERVER_DIR/server.properties $SERVER_DIR/whitelist.json $SERVER_DIR/ops.json
+tar -czf $BACKUP_FILE $SERVER_DIR/world $SERVER_DIR/server.properties $SERVER_DIR/whitelist.json $SERVER_DIR/ops.json
 
 # Only keep 7 days worth of backups
 find $BACKUP_DIR -type f -name 'minecraft_backup_*' -mtime +7 -exec rm {} \;
 
-# loop through all backups and only keep a single instance if they have the same sha256sum
+# loop through all backups and only keep a single instance if they have the same sha256sum, but don't delete the current backup
 for file in $(ls $BACKUP_DIR/minecraft_backup_*); do
-    if [ $(sha256sum $file | cut -d ' ' -f1) == $(sha256sum $BACKUP_FILE | cut -d ' ' -f1) ]; then
-        rm $BACKUP_FILE
-        break
+    if [ $file != $BACKUP_FILE ]; then
+        if [ $(sha256sum $file | cut -d ' ' -f1) == $(sha256sum $BACKUP_FILE | cut -d ' ' -f1) ]; then
+            rm $file
+        fi
     fi
 done
 
